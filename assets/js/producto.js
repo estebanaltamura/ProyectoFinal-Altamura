5fd9668320886052c45b35cd1b21430c00a584b3
yo <esteban.altamura@gmail.com>
2023-02-02 16:12:33 -0300

    prueba

--- assets/js/producto.js
+++ assets/js/producto.js
@@ -3,12 +3,12 @@ import { productPrinting } from "./productPrinting.js"
 
 const botonATC = document.getElementById("botonATC")
 
-let producto
+
 
 document.addEventListener("DOMContentLoaded", ()=>{
     const productInfo = JSON.parse(localStorage.getItem("infoToProductPage"))
     const {idProduct, colectionName} = productInfo
-    producto = fetchData(colectionName, idProduct)   
+    const producto = fetchData(colectionName, idProduct)   
     productPrinting(producto)
 }) 
 
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
 botonATC.addEventListener("click",()=>{
     
     localStorage.setItem("ultimoProductoAgregado", localStorage.getItem("infoToProductPage"));
-    window.location.href = `../../pages/carro.html` 
+    window.location.href = `../pages/carro.html` 
 })
 
 
