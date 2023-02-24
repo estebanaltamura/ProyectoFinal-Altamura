923ecd3506a013e038f4f2b1e2fb688035404fb0
yo <esteban.altamura@gmail.com>
2023-02-24 18:12:56 -0300

    prueba

--- assets/js/home.js
+++ assets/js/home.js
@@ -7,8 +7,6 @@ const showHome = ()=>{
     const footer = document.getElementById("footer")
     const loadingIcon = document.getElementById("loadingIcon")
 
-    alert("cargo")
-
     contenedorMain.className    == "hidden" && contenedorMain.classList.replace("hidden","contenedorMain")
     footer.className            == "hidden" && footer.classList.replace("hidden","footer")
     loadingIcon.className       == "loadingIcon" && loadingIcon.classList.replace("loadingIcon", "loadingIconOff")
