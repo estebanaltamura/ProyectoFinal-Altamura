3e1594d8455599e1b42895c25fc1c5c8f576b225
yo <esteban.altamura@gmail.com>
2023-02-24 18:02:15 -0300

    prueba

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,13 +1,17 @@
-const contenedorMain = document.getElementById("contenedorMain")
-const footer = document.getElementById("footer")
-const loadingIcon = document.getElementById("loadingIcon")
+
 
 window.addEventListener("load", ()=>showHome())
 
 const showHome = ()=>{
-        contenedorMain.className    == "hidden" && contenedorMain.classList.replace("hidden","contenedorMain")
-        footer.className            == "hidden" && footer.classList.replace("hidden","footer")
-        loadingIcon.className       == "loadingIcon" && loadingIcon.classList.replace("loadingIcon", "loadingIconOff")
+    const contenedorMain = document.getElementById("contenedorMain")
+    const footer = document.getElementById("footer")
+    const loadingIcon = document.getElementById("loadingIcon")
+
+    alert("cargo")
+
+    contenedorMain.className    == "hidden" && contenedorMain.classList.replace("hidden","contenedorMain")
+    footer.className            == "hidden" && footer.classList.replace("hidden","footer")
+    loadingIcon.className       == "loadingIcon" && loadingIcon.classList.replace("loadingIcon", "loadingIconOff")
 }
 
 
